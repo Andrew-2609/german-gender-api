2f24c410dba947be93325dfc43f556d1c941ede0
Andrew Monteiro <andrew.agoravai@gmail.com>
2021-12-09 22:52:26 -0300

    Feat: add HTTP response for not founded nouns

--- router/noun.py
+++ router/noun.py
@@ -1,5 +1,5 @@
 from typing import List
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 
 import database
@@ -21,4 +21,8 @@ def get_all_nouns(limit: int = 10, db: Session = Depends(database.get_db)):
 @router.get("/{noun}", response_model=schema.BaseNoun)
 def get_noun(searched_noun: str, db: Session = Depends(database.get_db)):
     found_noun = db.query(model.Noun).filter(model.Noun.noun == searched_noun).first()
+
+    if not found_noun:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Noun {searched_noun} was not found")
+
     return found_noun
