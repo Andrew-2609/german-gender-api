3d2722b00812755b5c04556f35e3310f70d351d6
Andrew Monteiro <andrew.agoravai@gmail.com>
2021-12-10 11:25:04 -0300

    Feat: add a detailed message for not found nouns

--- repository/noun.py
+++ repository/noun.py
@@ -2,6 +2,7 @@ from fastapi import HTTPException, status
 from sqlalchemy.orm import Session
 
 import model
+import router.noun
 
 
 def get_all(gender: model.Gender, db: Session):
@@ -18,6 +19,10 @@ def get_by_noun(noun: str, db: Session):
     found_noun = db.query(model.Noun).filter(model.Noun.noun == noun).first()
 
     if not found_noun:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Noun '{noun}' was not found")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail={
+            "message": f"Noun '{noun}' was not found",
+            "procedure": "If you are sure that it sould exist, please contact me at andrew.agoravai@gmail.com",
+            "url": router.noun.router.url_path_for("get_noun", **{"searched_noun": noun})
+        })
 
     return found_noun
