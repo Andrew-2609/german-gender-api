21c42d241554345bfe02578c5e5913eeea933f16
Andrew Monteiro <andrew.agoravai@gmail.com>
2021-12-11 18:20:20 -0300

    Docs: add documentation for router/noun.py endpoints

--- router/noun.py
+++ router/noun.py
@@ -5,6 +5,7 @@ from fastapi_pagination import Page, paginate, add_pagination
 from sqlalchemy.orm import Session
 
 import database
+import docs.noun
 import model
 import schema
 from repository import noun
@@ -15,14 +16,16 @@ router = APIRouter(
 )
 
 
-@router.get("/", status_code=status.HTTP_200_OK, response_model=Page[schema.NounWithArticles])
+@router.get("/", status_code=status.HTTP_200_OK, response_model=Page[schema.NounWithArticles],
+            responses=docs.noun.get_all_nouns_responses)
 def get_all_nouns(gender: Optional[model.Gender] = None, db: Session = Depends(database.get_db)):
     nouns = noun.get_all(gender, db)
     set_appropriate_articles(nouns)
     return paginate(nouns)
 
 
-@router.get("/{searched_noun}", status_code=status.HTTP_200_OK, response_model=schema.NounWithArticles)
+@router.get("/{searched_noun}", status_code=status.HTTP_200_OK, response_model=schema.NounWithArticles,
+            responses=docs.noun.get_noun_responses)
 def get_noun(searched_noun: str, db: Session = Depends(database.get_db)):
     found_noun = noun.get_by_noun(searched_noun.capitalize(), db)
     set_appropriate_articles(found_noun)
