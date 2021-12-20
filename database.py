fc4d4900888f4ca248202f3a270e7cfe13ffeed9
Andrew Monteiro <andrew.agoravai@gmail.com>
2021-12-20 16:25:00 -0300

    Feat: replace MySQL DATABASE_URL with SQLite DATABASE_URL

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "mysql+mysqlconnector://intellij:myMysqlPassword0502@172.17.0.2:3306/test_worterbuch"
+DATABASE_URL = "sqlite:///./worterbuch.db"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
