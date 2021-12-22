cd05989dbb8d097fb0caf146dcf1aa7bed08f489
Andrew Monteiro <andrew.agoravai@gmail.com>
2021-12-22 10:02:58 -0300

    Feat: replace basic implementation of index endpoint with markdown home.md

--- app/router/home.py
+++ app/router/home.py
@@ -2,6 +2,8 @@ from fastapi import APIRouter, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
+from library.helpers import openfile
+
 router = APIRouter()
 
 templates = Jinja2Templates(directory="templates")
@@ -9,4 +11,5 @@ templates = Jinja2Templates(directory="templates")
 
 @router.get("/", response_class=HTMLResponse, include_in_schema=False)
 async def index(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+    data = openfile("home.md")
+    return templates.TemplateResponse("index.html", {"request": request, "data": data})
