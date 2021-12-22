2041bbd724dc8828c643b2f60e23179e881eb830
Andrew Monteiro <andrew.agoravai@gmail.com>
2021-12-21 21:04:11 -0300

    Feat: add home router to main.py

--- app/main.py
+++ app/main.py
@@ -2,10 +2,11 @@ from fastapi import FastAPI
 
 import model
 from database import engine
-from router import noun
+from router import noun, home
 
 app = FastAPI()
 
 model.Base.metadata.create_all(engine)
 
 app.include_router(router=noun.router)
+app.include_router(router=home.router)
