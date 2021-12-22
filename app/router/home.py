30d70be3e7b85abecc7b6295f484791906cb1fd8
Andrew Monteiro <andrew.agoravai@gmail.com>
2021-12-22 07:05:04 -0300

    Refactor: remove comments in home router

--- app/router/home.py
+++ app/router/home.py
@@ -1,11 +1,8 @@
 from fastapi import APIRouter, Request
 from fastapi.responses import HTMLResponse
-# from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
 router = APIRouter()
-
-# router.mount("/static", StaticFiles(directory="static"), name="static")
 
 templates = Jinja2Templates(directory="templates")
 
