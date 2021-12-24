cad96a3721705176fa22c1b6fcd7bcdaf4ab4ad0
Andrew Monteiro <andrew.agoravai@gmail.com>
2021-12-23 22:02:33 -0300

    Chore: remove articles from nouns responses in router

--- app/router/noun.py
+++ app/router/noun.py
@@ -24,48 +24,11 @@ def get_all_nouns(
         db: Session = Depends(database.get_db)
 ):
     nouns = noun.get_all(gender, page, size, db)
-    set_appropriate_articles(nouns)
-
     return {"content": nouns, "page": page, "size": size}
 
 
-@router.get("/{searched_noun}", status_code=status.HTTP_200_OK, response_model=schema.NounWithArticles,
+@router.get("/{searched_noun}", status_code=status.HTTP_200_OK, response_model=schema.BaseNoun,
             responses=docs.noun.get_noun_responses)
 def get_noun(searched_noun: str, db: Session = Depends(database.get_db)):
     found_noun = noun.get_by_noun(searched_noun.capitalize(), db)
-    set_appropriate_articles(found_noun)
     return found_noun
-
-
-def set_appropriate_articles(noun_s):
-    if type(noun_s) == model.Noun:
-        check_noun_gender(noun_s)
-    elif type(noun_s) == list:
-        for entry in noun_s:
-            check_noun_gender(entry)
-
-
-def check_noun_gender(analysed_noun: model.Noun):
-    if analysed_noun.gender == model.Gender.masculine:
-        analysed_noun.articles = ({
-            "nominative": "der",
-            "accusative": "den",
-            "dative": "dem",
-            "genitive": "des"
-        })
-    elif analysed_noun.gender == model.Gender.feminine:
-        analysed_noun.articles = ({
-            "nominative": "die",
-            "accusative": "die",
-            "dative": "der",
-            "genitive": "der"
-        })
-    elif analysed_noun.gender == model.Gender.neuter:
-        analysed_noun.articles = ({
-            "nominative": "das",
-            "accusative": "das",
-            "dative": "dem",
-            "genitive": "des"
-        })
-    elif analysed_noun.gender == model.Gender.tbd:
-        analysed_noun.articles = {}
