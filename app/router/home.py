4ddf6f9ac41308cf43f5f16ee411e7750f7bafe3
Andrew Monteiro <andrew.agoravai@gmail.com>
2021-12-28 23:14:36 -0300

    Chore: remove helpers module and all implementation regarding home.md

--- app/router/home.py
+++ app/router/home.py
@@ -2,8 +2,6 @@ from fastapi import APIRouter, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
-from library.helpers import openfile
-
 router = APIRouter()
 
 templates = Jinja2Templates(directory="templates")
@@ -11,5 +9,4 @@ templates = Jinja2Templates(directory="templates")
 
 @router.get("/", response_class=HTMLResponse, include_in_schema=False)
 async def index(request: Request):
-    data = openfile("home.md")
-    return templates.TemplateResponse("index.html", {"request": request, "data": data})
+    return templates.TemplateResponse("index.html", {"request": request})
